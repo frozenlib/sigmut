d702c887cf301d7d0af24521a5c9ddd6103fd7d0
frozenlib <frozenlib@users.noreply.github.com>
2020-02-09 15:04:39 +0900

    Implement `map_async`.

--- src/bind.rs
+++ src/bind.rs
@@ -1,11 +1,14 @@
-use futures::task::LocalSpawn;
+use futures::future::RemoteHandle;
+use futures::task::{LocalSpawn, LocalSpawnExt};
 use std::any::Any;
 use std::cell::RefCell;
 use std::cmp::min;
+use std::future::Future;
 use std::mem::drop;
 use std::mem::replace;
 use std::ops::Deref;
 use std::rc::{Rc, Weak};
+use std::task::Poll;
 
 pub trait BindSource: 'static {
     fn sinks(&self) -> &BindSinks;
@@ -200,6 +203,12 @@ pub trait Bind: Sized + 'static {
 
     fn map<F: Fn(Self::Item) -> U, U>(self, f: F) -> Map<Self, F> {
         Map { b: self, f }
+    }
+    fn map_async<F: Fn(Self::Item) -> Fut + 'static, Fut: Future + 'static>(
+        self,
+        f: F,
+    ) -> MapAsync<Self, F, Fut> {
+        MapAsync::new(self, f)
     }
 }
 
@@ -503,6 +512,100 @@ impl<B: RefBind, F: Fn(&B::Item) -> &U + 'static, U> RefBind for RefMapRef<B, F>
 
     fn bind(&self, ctx: &mut BindContext) -> Ref<Self::Item> {
         Ref::map(self.b.bind(ctx), &self.f)
+    }
+}
+
+pub struct MapAsync<B: Bind, F: Fn(B::Item) -> Fut, Fut: Future>(Rc<MapAsyncData<B, F, Fut>>);
+
+struct MapAsyncData<B: Bind, F: Fn(B::Item) -> Fut, Fut: Future> {
+    sp: Rc<dyn LocalSpawn>,
+    b: B,
+    f: F,
+    sinks: BindSinks,
+    state: RefCell<MapAsyncState<Fut::Output>>,
+}
+struct MapAsyncState<T> {
+    value: Poll<T>,
+    handle: Option<RemoteHandle<()>>,
+    binds: Vec<Binding>,
+}
+
+impl<B: Bind, F: Fn(B::Item) -> Fut + 'static, Fut: Future<Output = U> + 'static, U>
+    MapAsync<B, F, Fut>
+{
+    fn new(b: B, f: F) -> Self {
+        MapAsync(Rc::new(MapAsyncData {
+            sp: get_current_local_spawn(),
+            b,
+            f,
+            sinks: BindSinks::new(),
+            state: RefCell::new(MapAsyncState {
+                value: Poll::Pending,
+                handle: None,
+                binds: Vec::new(),
+            }),
+        }))
+    }
+
+    fn ready(&self) {
+        let mut s = self.0.state.borrow_mut();
+        let mut ctx = BindContext::new(&self.0, &mut s.binds);
+        let value = self.0.b.bind(&mut ctx);
+        let fut = (self.0.f)(value);
+        let this = Rc::downgrade(&self.0);
+        s.handle = Some(
+            self.0
+                .sp
+                .spawn_local_with_handle(async move {
+                    let value = fut.await;
+                    if let Some(this) = Weak::upgrade(&this) {
+                        let mut s = this.state.borrow_mut();
+                        s.value = Poll::Ready(value);
+                        drop(s);
+                        this.sinks.notify();
+                    }
+                })
+                .unwrap(),
+        );
+    }
+}
+
+impl<B: Bind, F: Fn(B::Item) -> Fut + 'static, Fut: Future<Output = U> + 'static, U> RefBind
+    for MapAsync<B, F, Fut>
+{
+    type Item = Poll<U>;
+
+    fn bind(&self, ctx: &mut BindContext) -> Ref<Self::Item> {
+        let mut s = self.0.state.borrow();
+        if s.handle.is_none() {
+            drop(s);
+            self.ready();
+            s = self.0.state.borrow();
+        }
+        ctx.bind(self.0.clone());
+        Ref::map(Ref::Cell(s), |o| &o.value)
+    }
+}
+impl<B: Bind, F: Fn(B::Item) -> Fut + 'static, Fut: Future<Output = U> + 'static, U> BindSource
+    for MapAsyncData<B, F, Fut>
+{
+    fn sinks(&self) -> &BindSinks {
+        &self.sinks
+    }
+}
+impl<B: Bind, F: Fn(B::Item) -> Fut + 'static, Fut: Future<Output = U> + 'static, U> BindSink
+    for MapAsyncData<B, F, Fut>
+{
+    fn notify(self: Rc<Self>, ctx: &NotifyContext) {
+        let mut s = self.state.borrow_mut();
+        if s.handle.is_some() {
+            s.handle = None;
+            if let Poll::Ready(_) = &s.value {
+                s.value = Poll::Pending;
+                drop(s);
+                self.sinks.notify_with(ctx);
+            }
+        }
     }
 }
 
