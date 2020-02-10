a4fc1d133e24590e4fce4c4459bafec6dd278e03
frozenlib <frozenlib@users.noreply.github.com>
2020-02-10 20:46:12 +0900

    Split to `dyn_bind` module.

---
+++ src/dyn_bind.rs
@@ -0,0 +1,51 @@
+use crate::*;
+use std::any::Any;
+use std::rc::Rc;
+
+pub trait DynBind: 'static {
+    type Item;
+
+    fn dyn_bind(self: Rc<Self>, ctx: &mut BindContext) -> Self::Item;
+}
+pub trait DynRefBind: 'static {
+    type Item;
+
+    fn dyn_bind(&self, rc_this: &dyn Any, ctx: &mut BindContext) -> Ref<Self::Item>;
+    fn downcast(rc_this: &dyn Any) -> &Rc<Self>
+    where
+        Self: Sized + 'static,
+    {
+        rc_this.downcast_ref().unwrap()
+    }
+}
+
+impl<B: Bind> DynBind for B {
+    type Item = B::Item;
+    fn dyn_bind(self: Rc<Self>, ctx: &mut BindContext) -> Self::Item {
+        self.bind(ctx)
+    }
+}
+impl<B: RefBind> DynRefBind for B {
+    type Item = B::Item;
+
+    fn dyn_bind(&self, _rc_this: &dyn Any, ctx: &mut BindContext) -> Ref<Self::Item> {
+        self.bind(ctx)
+    }
+}
+pub type RcBind<T> = Rc<dyn DynBind<Item = T>>;
+pub type RcRefBind<T> = Rc<dyn DynRefBind<Item = T>>;
+
+impl<T: 'static> Bind for RcBind<T> {
+    type Item = T;
+
+    fn bind(&self, ctx: &mut BindContext) -> Self::Item {
+        self.clone().dyn_bind(ctx)
+    }
+}
+impl<T: 'static> RefBind for RcRefBind<T> {
+    type Item = T;
+
+    fn bind(&self, ctx: &mut BindContext) -> Ref<Self::Item> {
+        self.dyn_bind(self, ctx)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod bind;
 mod bind_ext;
 mod cell;
+mod dyn_bind;
 
 pub use bind::*;
 pub use bind_ext::*;
 pub use cell::*;
+pub use dyn_bind::*;
