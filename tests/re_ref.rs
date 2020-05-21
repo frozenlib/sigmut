612192100a278a75d2e440a70dd952ebf20b902a
frozenlib <frozenlib@users.noreply.github.com>
2020-05-21 11:45:58 +0900

    Add tests.

--- tests/re_ref.rs
+++ tests/re_ref.rs
@@ -20,3 +20,21 @@ fn re_ref_new() {
 
     assert_eq!(r.finish(), vec![2, 5, 7]);
 }
+
+#[test]
+fn re_ref_new_cell2() {
+    let cell1 = ReCell::new(1);
+    let cell2 = ReCell::new(2);
+
+    let re = ReRef::new(
+        (cell1.clone(), cell2.clone()),
+        move |(cell1, cell2), ctx, f| f(&(cell1.get(ctx) + cell2.get(ctx))),
+    )
+    .cloned();
+    let r = record(&re);
+
+    cell1.set_and_update(5);
+    cell2.set_and_update(10);
+
+    assert_eq!(r.finish(), vec![1 + 2, 5 + 2, 5 + 10]);
+}
