94a67e2ba3efe46c860603ffc13265c1918c396b
frozenlib <frozenlib@users.noreply.github.com>
2020-05-21 19:01:02 +0900

    Add tests for `ReBorrow::flat_map`.

--- tests/re_borrow.rs
+++ tests/re_borrow.rs
@@ -37,3 +37,26 @@ fn re_borrow_map_ref() {
 
     assert_eq!(r.stop(), vec![2, 5, 7]);
 }
+
+#[test]
+fn re_borrow_flat_map() {
+    let a = [ReCell::new(5), ReCell::new(10)];
+    let a_ = a.clone();
+
+    let b = ReRefCell::new(0);
+
+    let r = b.to_re_borrow().flat_map(move |&x| a_[x].to_re()).to_vec();
+
+    a[0].set_and_update(6);
+    a[1].set_and_update(12);
+
+    a[0].set_and_update(7);
+    a[1].set_and_update(13);
+
+    b.set_and_update(1);
+
+    a[0].set_and_update(8);
+    a[1].set_and_update(14);
+
+    assert_eq!(r.stop(), vec![5, 6, 7, 13, 14]);
+}
