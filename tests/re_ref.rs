deed8439a1132090f083f68e69bb6e754bf43b03
frozenlib <frozenlib@users.noreply.github.com>
2020-05-21 10:54:10 +0900

    Add tests for `ReRef::new`.

--- tests/re_ref.rs
+++ tests/re_ref.rs
@@ -9,3 +9,14 @@ fn re_ref_constant() {
     let r = record(&a.cloned());
     assert_eq!(r.finish(), vec![2]);
 }
+#[test]
+fn re_ref_new() {
+    let a = ReCell::new(2);
+    let b = ReRef::new(a.clone(), move |a, ctx, f| f(&a.get(ctx)));
+    let r = record(&b.cloned());
+
+    a.set_and_update(5);
+    a.set_and_update(7);
+
+    assert_eq!(r.finish(), vec![2, 5, 7]);
+}
