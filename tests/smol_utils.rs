fdc79aa6a9d9f379081a0ad80a1fbbb5108025cf
frozenlib <frozenlib@users.noreply.github.com>
2020-05-22 23:35:02 +0900

    Add tests for `re_map_async`.

--- tests/smol_utils.rs
+++ tests/smol_utils.rs
@@ -6,29 +6,78 @@ use futures::{
 use reactive_fn::smol_utils::*;
 use reactive_fn::*;
 use smol::{Task, Timer};
-use std::time::Duration;
+use std::{fmt::Debug, task::Poll, time::Duration};
+use thiserror::Error;
 
-async fn assert_timeout(fut: impl Future + Unpin) {
-    let dur = Duration::from_millis(600);
-    let res = select(fut, Timer::after(dur)).await;
-    assert!(matches!(res, Either::Right(_)), "not timeouted");
+const NEXT_DURATION: Duration = Duration::from_millis(300);
+
+fn send_values<T: 'static + Copy>(cell: &ReCell<T>, values: Vec<T>) -> Task<()> {
+    let cell = cell.clone();
+    Task::local(async move {
+        for value in values {
+            Timer::after(NEXT_DURATION).await;
+            cell.set_and_update(value);
+        }
+    })
+}
+async fn assert_values<T>(source: Re<T>, values: Vec<T>)
+where
+    T: 'static + PartialEq + Debug,
+{
+    let mut s = source.to_stream();
+    let dur = NEXT_DURATION * 2;
+    for value in values {
+        assert_eq!(timeout(s.next(), dur).await, Ok(Some(value)));
+    }
+    assert_eq!(timeout(s.next(), dur).await, Err(TimeoutError));
+}
+
+#[derive(Error, Debug, Eq, PartialEq)]
+#[error("timeout")]
+struct TimeoutError;
+
+async fn timeout<T>(
+    fut: impl Future<Output = T> + Unpin,
+    dur: Duration,
+) -> std::result::Result<T, TimeoutError> {
+    match select(fut, Timer::after(dur)).await {
+        Either::Left(x) => Ok(x.0),
+        Either::Right(_) => Err(TimeoutError),
+    }
 }
 
 #[test]
 fn re_to_stream() {
     smol::run(async {
-        let cell = ReCell::new(1u64);
-        let mut s = cell.to_re().to_stream();
-        let dur = Duration::from_millis(300);
-        let _task = Task::local(async move {
-            Timer::after(dur).await;
-            cell.set_and_update(5);
-            Timer::after(dur).await;
-            cell.set_and_update(6);
-        });
-        assert_eq!(s.next().await, Some(1));
-        assert_eq!(s.next().await, Some(5));
-        assert_eq!(s.next().await, Some(6));
-        assert_timeout(s.next()).await;
+        let cell = ReCell::new(1);
+        let _task = send_values(&cell, vec![5, 6]);
+        assert_values(cell.to_re(), vec![1, 5, 6]).await;
+    });
+}
+
+#[test]
+fn re_map_async() {
+    smol::run(async {
+        let cell = ReCell::new(1);
+        let r = cell
+            .to_re()
+            .map_async_with(
+                |x| async move {
+                    Timer::after(NEXT_DURATION / 2).await;
+                    x + 2
+                },
+                LocalSpawner,
+            )
+            .cloned();
+        let _task = send_values(&cell, vec![5, 10]);
+        let values = vec![
+            Poll::Pending,
+            Poll::Ready(3),
+            Poll::Pending,
+            Poll::Ready(7),
+            Poll::Pending,
+            Poll::Ready(12),
+        ];
+        assert_values(r, values).await;
     });
 }
