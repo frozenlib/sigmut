08474e133d0eec8db26b93a9283cdac07f51fbac
frozenlib <frozenlib@users.noreply.github.com>
2021-01-14 08:29:19 +0900

    Change definition of `Sink`.

--- reactive-fn/src/sink.rs
+++ reactive-fn/src/sink.rs
@@ -1,23 +1,8 @@
 use crate::*;
 use std::rc::Rc;
 
-pub struct Sink<T>(Option<Rc<dyn Observer<T>>>);
+pub struct DynObserver<T>(Option<Rc<dyn Observer<T>>>);
 
-impl<T> Sink<T> {
-    pub fn null() -> Self {
-        Self(None)
-    }
-    pub fn new(o: impl Observer<T>) -> Self {
-        todo!()
-    }
-}
-
-pub trait IntoSink<T> {
-    fn into_sink(self) -> Sink<T>;
-}
-
-impl<T, S: IntoSink<T>> IntoSink<T> for DynObs<S> {
-    fn into_sink(self) -> Sink<T> {
-        todo!()
-    }
+pub trait Sink<T> {
+    fn connect(self, value: T) -> DynObserver<T>;
 }
