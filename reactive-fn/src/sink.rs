fac85a95e0b8046537f73c6a821498170526ded6
frozenlib <frozenlib@users.noreply.github.com>
2021-01-14 08:37:37 +0900

    Implementing `Sink`.

--- reactive-fn/src/sink.rs
+++ reactive-fn/src/sink.rs
@@ -6,3 +6,11 @@ pub struct DynObserver<T>(Option<Rc<dyn Observer<T>>>);
 pub trait Sink<T> {
     fn connect(self, value: T) -> DynObserver<T>;
 }
+
+// impl<S: Observable<Item = T>, T: Sink<T>> Sink<T> for Obs<S> {
+//     fn connect(self, value: T) -> DynObserver<T> {
+//         let (head, tail) = self.head_tail();
+//         let o = head.connect(value);
+
+//     }
+// }
