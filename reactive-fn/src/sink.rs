086cc5658f75b15d1e596aba5cf9f55b4fe59ab8
frozenlib <frozenlib@users.noreply.github.com>
2021-01-14 16:19:11 +0900

    Fix wrong `Sink<T>`  implementation.

--- reactive-fn/src/sink.rs
+++ reactive-fn/src/sink.rs
@@ -53,27 +53,81 @@ where
     S: Sink<T>,
 {
     fn connect(self, value: T) -> DynObserver<T> {
+        (&self).connect(value)
+    }
+}
+impl<T, S> Sink<T> for &DynObs<S>
+where
+    T: Clone + 'static,
+    S: Sink<T>,
+{
+    fn connect(self, value: T) -> DynObserver<T> {
         self.obs().connect(value)
     }
 }
-impl<T> Sink<T> for DynObsBorrow<T>
+
+impl<T, S> Sink<T> for DynObsBorrow<S>
 where
     T: Clone + 'static,
-    for<'a> &'a T: Sink<T>,
+    for<'a> &'a S: Sink<T>,
+{
+    fn connect(self, value: T) -> DynObserver<T> {
+        (&self).connect(value)
+    }
+}
+impl<T, S> Sink<T> for &DynObsBorrow<S>
+where
+    T: Clone + 'static,
+    for<'a> &'a S: Sink<T>,
 {
     fn connect(self, value: T) -> DynObserver<T> {
         self.as_ref().connect(value)
     }
 }
-impl<T> Sink<T> for DynObsRef<T>
+
+impl<T, S> Sink<T> for DynObsRef<S>
 where
     T: Clone + 'static,
-    for<'a> &'a T: Sink<T>,
+    for<'a> &'a S: Sink<T>,
+{
+    fn connect(self, value: T) -> DynObserver<T> {
+        (&self).connect(value)
+    }
+}
+impl<T, S> Sink<T> for &DynObsRef<S>
+where
+    T: Clone + 'static,
+    for<'a> &'a S: Sink<T>,
 {
     fn connect(self, value: T) -> DynObserver<T> {
         self.obs().connect(value)
     }
 }
+
+// impl<T,S> Sink<T> for Source<S>
+// where
+//     T: Clone + 'static,
+//     for<'a> &'a T: Sink<T>,
+// {
+//     fn connect(self, value: T) -> DynObserver<T> {
+//         match self {
+//             Source::Constant(c) => c.connect(value),
+//             Source::Obs(o) => o.connect(value),
+//         }
+//     }
+// }
+// impl<T> Sink<T> for &Source<T>
+// where
+//     T: Clone + 'static,
+//     for<'a> &'a T: Sink<T>,
+// {
+//     fn connect(self, value: T) -> DynObserver<T> {
+//         match self {
+//             Source::Constant(c) => c.connect(value),
+//             Source::Obs(o) => o.as_ref().connect(value),
+//         }
+//     }
+// }
 
 struct InnerObserver<T> {
     value: T,
