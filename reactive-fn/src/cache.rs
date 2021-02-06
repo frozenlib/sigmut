22476778c732f5b26b0962a1759b377701b396cc
frozenlib <frozenlib@users.noreply.github.com>
2021-02-06 15:19:22 +0900

    Add `Cache::cache`, `Cache::cache_mut`.

--- reactive-fn/src/cache.rs
+++ reactive-fn/src/cache.rs
@@ -35,13 +35,11 @@ impl<T: 'static> Cache<T> {
         cx: &mut BindContext,
     ) -> RefMut<T> {
         self.load(f, cx.scope());
-        self.bind(cx);
-        self.try_borrow_mut().unwrap()
+        self.try_borrow_mut(cx).unwrap()
     }
     pub fn borrow(&self, f: impl FnOnce(&mut BindContext) -> T, cx: &mut BindContext) -> Ref<T> {
         self.load(f, cx.scope());
-        self.bind(cx);
-        self.try_borrow().unwrap()
+        self.try_borrow(cx).unwrap()
     }
     fn load(&self, f: impl FnOnce(&mut BindContext) -> T, scope: &BindScope) {
         if self.0.state.borrow().value.is_some() {
@@ -50,10 +48,15 @@ impl<T: 'static> Cache<T> {
         let mut b = self.0.state.borrow_mut();
         b.value = Some(b.bindings.update(scope, &self.0, f));
     }
-    pub fn bind(&self, cx: &mut BindContext) {
+    pub fn try_borrow_mut(&self, cx: &mut BindContext) -> Option<RefMut<T>> {
         cx.bind(self.0.clone());
+        self.cache_mut()
     }
-    pub fn try_borrow_mut(&self) -> Option<RefMut<T>> {
+    pub fn try_borrow(&self, cx: &mut BindContext) -> Option<Ref<T>> {
+        cx.bind(self.0.clone());
+        self.cache()
+    }
+    pub fn cache_mut(&self) -> Option<RefMut<T>> {
         let r = RefMut::map(self.0.state.borrow_mut(), |x| &mut x.value);
         if r.is_some() {
             Some(RefMut::map(r, |x| x.as_mut().unwrap()))
@@ -61,7 +64,7 @@ impl<T: 'static> Cache<T> {
             None
         }
     }
-    pub fn try_borrow(&self) -> Option<Ref<T>> {
+    pub fn cache(&self) -> Option<Ref<T>> {
         let r = Ref::map(self.0.state.borrow(), |x| &x.value);
         if r.is_some() {
             Some(Ref::map(r, |x| x.as_ref().unwrap()))
