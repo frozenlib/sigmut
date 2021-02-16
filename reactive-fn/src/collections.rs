8e32ab0b264444f6b9f735a0fb983069420b4f92
frozenlib <frozenlib@users.noreply.github.com>
2021-02-16 11:44:31 +0900

    Add `DynObsList`.

--- reactive-fn/src/collections.rs
+++ reactive-fn/src/collections.rs
@@ -1,9 +1,11 @@
 mod list_change;
 mod shared_array;
 
+pub mod dyn_obs_list;
 pub mod obs_list;
 pub mod source_list;
 
+pub use dyn_obs_list::{DynObsList, DynObsListAge};
 pub use list_change::*;
 pub use obs_list::{ObsList, ObsListAge};
 pub use shared_array::*;
---
+++ reactive-fn/src/collections/dyn_obs_list.rs
@@ -0,0 +1,97 @@
+use crate::*;
+use std::{any::Any, iter::FusedIterator, ops::Index, rc::Rc};
+
+pub(crate) trait DynamicObservableList<T> {
+    fn borrow(&self, since: &DynObsListAge) -> Box<dyn DynamicObservableListRef<T>>;
+}
+pub(crate) trait DynamicObservableListRef<T> {
+    fn age(&self) -> DynObsListAge;
+    fn len(&self) -> usize;
+    fn get(&self, index: usize) -> Option<&T>;
+    fn changes<'a>(&self, f: &dyn FnMut(ListChange<&'a T>));
+}
+
+#[derive(Derivative)]
+#[derivative(Clone(bound = ""))]
+pub struct DynObsList<T>(Rc<dyn DynamicObservableList<T>>);
+pub struct Ref<T>(Box<dyn DynamicObservableListRef<T>>);
+
+#[derive(Derivative)]
+#[derivative(Clone(bound = ""))]
+pub enum DynObsListAge {
+    Empty,
+    Last,
+    Obs(Rc<dyn Any>),
+}
+
+impl<T> DynObsList<T> {
+    pub fn borrow(&self, since: &DynObsListAge) -> Ref<T> {
+        Ref(self.0.borrow(since))
+    }
+}
+impl<T> Ref<T> {
+    pub fn age(&self) -> DynObsListAge {
+        self.0.age()
+    }
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    pub fn get(&self, index: usize) -> Option<&T> {
+        self.0.get(index)
+    }
+    pub fn changes<'a>(&'a self, f: &dyn FnMut(ListChange<&'a T>)) {
+        self.0.changes(f)
+    }
+    pub fn iter(&self) -> Iter<T> {
+        Iter::new(self)
+    }
+}
+impl<T> Index<usize> for Ref<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        self.get(index).expect("out of index.")
+    }
+}
+impl<'a, T> IntoIterator for &'a Ref<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+pub struct Iter<'a, T> {
+    s: &'a Ref<T>,
+    index: usize,
+    s_len: usize,
+}
+
+impl<'a, T> Iter<'a, T> {
+    fn new(s: &'a Ref<T>) -> Self {
+        Self {
+            s,
+            index: 0,
+            s_len: s.len(),
+        }
+    }
+}
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let value = self.s.get(self.index)?;
+        self.index += 1;
+        Some(value)
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.s_len - self.index;
+        (len, Some(len))
+    }
+}
+impl<'a, T> ExactSizeIterator for Iter<'a, T> {}
+impl<'a, T> FusedIterator for Iter<'a, T> {}
