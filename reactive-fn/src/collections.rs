55610e31aeaeab9aba957df7c741fda41ec598df
frozenlib <frozenlib@users.noreply.github.com>
2021-02-17 18:51:59 +0900

    Remove `SharedArray`.

--- reactive-fn/src/collections.rs
+++ reactive-fn/src/collections.rs
@@ -1,5 +1,4 @@
 mod list_change;
-mod shared_array;
 
 pub mod dyn_obs_list;
 pub mod obs_list_cell;
@@ -8,7 +7,6 @@ pub mod source_list;
 pub use dyn_obs_list::{DynObsList, DynObsListAge};
 pub use list_change::*;
 pub use obs_list_cell::{ObsListCell, ObsListCellAge};
-pub use shared_array::*;
 pub use source_list::SourceList;
 
 pub(crate) use dyn_obs_list::{DynamicObservableList, DynamicObservableListRef};
