a1b963427af6495758c7f0547afbd8b6eec44617
frozenlib <frozenlib@users.noreply.github.com>
2022-05-30 00:50:44 +0900

    Simplify `Cache`.

--- reactive-fn/src/cache.rs
+++ reactive-fn/src/cache.rs
@@ -1,8 +1,12 @@
 use super::*;
+use derive_ex::derive_ex;
 use std::{
-    cell::{Ref, RefCell, RefMut},
+    cell::{Ref, RefCell},
     rc::Rc,
 };
+
+#[derive_ex(Default)]
+#[default(Self::new())]
 pub struct Cache<T>(Rc<CacheData<T>>);
 
 struct CacheData<T> {
@@ -15,7 +19,7 @@ struct CacheState<T> {
     bindings: Bindings,
 }
 
-impl<T: 'static> Cache<T> {
+impl<T> Cache<T> {
     pub fn new() -> Self {
         Self(Rc::new(CacheData {
             sinks: BindSinks::new(),
@@ -24,21 +28,6 @@ impl<T: 'static> Cache<T> {
                 bindings: Bindings::new(),
             }),
         }))
-    }
-    pub fn borrow(&self, f: impl FnOnce(&mut BindContext) -> T, bc: &mut BindContext) -> Ref<T> {
-        self.load(f, bc.scope());
-        self.try_borrow(bc).unwrap()
-    }
-    fn load(&self, f: impl FnOnce(&mut BindContext) -> T, scope: &BindScope) {
-        if self.0.state.borrow().value.is_some() {
-            return;
-        }
-        let mut b = self.0.state.borrow_mut();
-        b.value = Some(b.bindings.update(scope, &self.0, f));
-    }
-    fn try_borrow(&self, bc: &mut BindContext) -> Option<Ref<T>> {
-        bc.bind(self.0.clone());
-        self.cache()
     }
     pub fn is_cached(&self) -> bool {
         self.0.state.borrow().value.is_some()
@@ -51,18 +40,23 @@ impl<T: 'static> Cache<T> {
             None
         }
     }
-    pub fn clear(&self) {
-        if self.0.state.borrow_mut().value.take().is_some() {
-            schedule_notify(&self.0)
+}
+
+impl<T: 'static> Cache<T> {
+    pub fn borrow(&self, f: impl FnOnce(&mut BindContext) -> T, bc: &mut BindContext) -> Ref<T> {
+        self.load(f, bc.scope());
+        bc.bind(self.0.clone());
+        self.cache().unwrap()
+    }
+    fn load(&self, f: impl FnOnce(&mut BindContext) -> T, scope: &BindScope) {
+        if self.0.state.borrow().value.is_some() {
+            return;
         }
+        let mut b = self.0.state.borrow_mut();
+        b.value = Some(b.bindings.update(scope, &self.0, f));
     }
 }
 
-impl<T: 'static> Default for Cache<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 impl<T: 'static> BindSink for CacheData<T> {
     fn notify(self: Rc<Self>, scope: &NotifyScope) {
         if self.state.borrow_mut().value.take().is_some() {
