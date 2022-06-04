a2055a2c3ffcea2bc0e7a72aa183fdf16e03af0e
frozenlib <frozenlib@users.noreply.github.com>
2022-06-04 11:49:06 +0900

    Add `Action`.

--- reactive-fn/src/core/idle_task.rs
+++ reactive-fn/src/core/idle_task.rs
@@ -1,29 +1,66 @@
 use rt_local_core::{spawn_local, wait_for_idle};
-use std::{cell::RefCell, mem::swap, rc::Rc, task::Waker};
+use std::{any::Any, cell::RefCell, mem::swap, rc::Rc, task::Waker};
+
+pub struct Action {
+    this: Rc<dyn Any>,
+    f: Box<dyn Fn(Rc<dyn Any>)>,
+}
+impl Action {
+    /// Create a new action.
+    ///
+    /// Should be zero sized type for `f` to avoid heap allocation.
+    pub fn new<T, F>(this: Rc<T>, f: F) -> Action
+    where
+        T: 'static,
+        F: Fn(Rc<T>) + 'static,
+    {
+        Action {
+            this,
+            f: Box::new(move |this| f(this.downcast::<T>().unwrap())),
+        }
+    }
+    pub fn schedule_normal(self) {
+        TaskRunner::with_normal(|runner| runner.push_task(self));
+    }
+    pub fn schedule_idle(self) {
+        TaskRunner::with_idle(|runner| runner.push_task(self));
+    }
+
+    fn run(self) {
+        (self.f)(self.this)
+    }
+}
 
 pub trait IdleTask: 'static {
     fn run(self: Rc<Self>);
 }
 pub fn schedule_idle(task: &Rc<impl IdleTask>) {
-    IdleTaskRunner::with(|r| r.push_task(task.clone()));
+    Action::new(task.clone(), IdleTask::run);
 }
 
 thread_local! {
-    static IDLE_TASK_RUNNER: RefCell<Option<IdleTaskRunner>> = RefCell::new(None);
+    static TASK_RUNNER_IDLE: RefCell<Option<TaskRunner>> = RefCell::new(None);
+    static TASK_RUNNER_NORMAL: RefCell<Option<TaskRunner>> = RefCell::new(None);
 }
 
-struct IdleTaskRunner {
-    tasks: Vec<Rc<dyn IdleTask>>,
+struct TaskRunner {
+    tasks: Vec<Action>,
     waker: Option<Waker>,
 }
 
-impl IdleTaskRunner {
-    fn new() -> Self {
-        spawn_local(async {
+impl TaskRunner {
+    fn new(is_idle: bool) -> Self {
+        spawn_local(async move {
             let mut tasks = Vec::new();
             loop {
-                wait_for_idle().await;
-                Self::with(|r| swap(&mut tasks, &mut r.tasks));
+                if is_idle {
+                    wait_for_idle().await;
+                }
+                if is_idle {
+                    Self::with_idle(|r| swap(&mut tasks, &mut r.tasks));
+                } else {
+                    Self::with_normal(|r| swap(&mut tasks, &mut r.tasks));
+                }
                 for task in tasks.drain(..) {
                     task.run();
                 }
@@ -36,10 +73,13 @@ impl IdleTaskRunner {
         }
     }
 
-    fn with<T>(f: impl FnOnce(&mut Self) -> T) -> T {
-        IDLE_TASK_RUNNER.with(|r| f(r.borrow_mut().get_or_insert_with(IdleTaskRunner::new)))
+    fn with_normal<T>(f: impl FnOnce(&mut Self) -> T) -> T {
+        TASK_RUNNER_NORMAL.with(|r| f(r.borrow_mut().get_or_insert_with(|| TaskRunner::new(false))))
     }
-    fn push_task(&mut self, task: Rc<dyn IdleTask>) {
+    fn with_idle<T>(f: impl FnOnce(&mut Self) -> T) -> T {
+        TASK_RUNNER_IDLE.with(|r| f(r.borrow_mut().get_or_insert_with(|| TaskRunner::new(true))))
+    }
+    fn push_task(&mut self, task: Action) {
         self.tasks.push(task);
         if let Some(waker) = self.waker.take() {
             waker.wake();
