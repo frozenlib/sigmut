20bddb7c425bcc4c246cfa5563546960d9b96d64
frozenlib <frozenlib@users.noreply.github.com>
2023-10-16 08:11:42 +0900

    Add `Unsubscribe`.

--- reactive-fn/src/observable/subscription.rs
+++ reactive-fn/src/observable/subscription.rs
@@ -7,13 +7,17 @@ use crate::{
 };
 use derive_ex::derive_ex;
 use futures::Future;
-use std::{any::Any, cell::RefCell, rc::Rc};
+use std::{
+    any::Any,
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
 
 const SLOT: usize = 0;
 
 #[derive_ex(Default)]
 #[default(Self::empty())]
-pub struct Subscription(Option<Rc<dyn Any>>);
+pub struct Subscription(SubscriptionInner);
 
 impl Subscription {
     pub fn new(mut f: impl FnMut(&mut ObsContext) + 'static) -> Self {
@@ -31,7 +35,7 @@ impl Subscription {
         })));
         let node = Rc::downgrade(&rc);
         schedule_update(node, SLOT);
-        Self(Some(rc))
+        Self::from_rc(rc)
     }
     pub fn new_future<Fut>(f: impl FnMut(&mut ObsContext) -> Fut + 'static) -> Subscription
     where
@@ -44,15 +48,56 @@ impl Subscription {
     where
         Fut: Future<Output = ()> + 'static,
     {
-        Subscription(Some(FromAsync::new(f, true)))
+        Self::from_rc(FromAsync::new(f, true))
     }
 
     pub fn empty() -> Self {
-        Self(None)
+        Self(SubscriptionInner::None)
     }
     pub fn from_rc(rc: Rc<dyn Any>) -> Self {
-        Self(Some(rc))
+        Self(SubscriptionInner::RcAny(rc))
     }
+    pub fn from_rc_slot(rc: Rc<dyn Unsubscribe>, slot: usize) -> Self {
+        Self(SubscriptionInner::RcUnsubscribe { rc, slot })
+    }
+    pub fn from_weak_slot(weak: Weak<dyn Unsubscribe>, slot: usize) -> Self {
+        Self(SubscriptionInner::WeakUnsubscribe { weak, slot })
+    }
+    pub fn unsubscribe(&mut self) {
+        *self = Self::empty();
+    }
+}
+impl Drop for Subscription {
+    fn drop(&mut self) {
+        match &mut self.0 {
+            SubscriptionInner::None => {}
+            SubscriptionInner::RcAny(_) => {}
+            SubscriptionInner::RcUnsubscribe { rc, slot } => {
+                rc.clone().unsubscribe(*slot);
+            }
+            SubscriptionInner::WeakUnsubscribe { weak, slot } => {
+                if let Some(rc) = weak.upgrade() {
+                    rc.unsubscribe(*slot);
+                }
+            }
+        }
+    }
+}
+
+enum SubscriptionInner {
+    None,
+    RcAny(Rc<dyn Any>),
+    RcUnsubscribe {
+        rc: Rc<dyn Unsubscribe>,
+        slot: usize,
+    },
+    WeakUnsubscribe {
+        weak: Weak<dyn Unsubscribe>,
+        slot: usize,
+    },
+}
+pub trait Unsubscribe {
+    fn unsubscribe(self: Rc<Self>, slot: usize);
 }
 
 struct Data<F> {
