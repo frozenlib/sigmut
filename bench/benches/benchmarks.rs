0eea5b72739f58ea34bc334780bf66a4fe00a5da
frozenlib <frozenlib@users.noreply.github.com>
2026-01-10 14:59:06 +0900

    Add benchmarks.

--- bench/benches/benchmarks.rs
+++ bench/benches/benchmarks.rs
@@ -1,29 +1,106 @@
-#![cfg(feature = "benchmark")]
 #![feature(test)]
 
-use std::hint::black_box;
+use std::{hint::black_box, rc::Rc};
+
+use sigmut::{State, effect};
 
 extern crate test;
 
 #[bench]
 fn one_to_one(b: &mut test::Bencher) {
     let mut rt = sigmut::core::Runtime::new();
-    let mut states = Vec::new();
-    let mut subscriptions = Vec::new();
-    for i in 0..100 {
+    let mut ss = Vec::new();
+    let mut subs = Vec::new();
+    for i in 0..10000 {
         let s = sigmut::State::new(i);
         let sub = s.to_signal().effect(|v| {
             black_box(v);
         });
-        states.push(s);
-        subscriptions.push(sub);
+        ss.push(s);
+        subs.push(sub);
     }
     b.iter(|| {
-        for n in 0..10 {
+        for n in 0..5 {
             let ac = rt.ac();
-            for s in &states {
+            for s in &ss {
                 s.set(n, ac);
             }
+            rt.update();
+        }
+    });
+}
+
+#[bench]
+fn one_to_many(b: &mut test::Bencher) {
+    let mut rt = sigmut::core::Runtime::new();
+    let mut subs = Vec::new();
+    let s = State::new(0);
+    for j in 0..10000 {
+        let sub = s.to_signal().effect(move |v| {
+            black_box((j, v));
+        });
+        subs.push(sub);
+    }
+
+    b.iter(|| {
+        for n in 0..5 {
+            let ac = rt.ac();
+            s.set(n, ac);
+            rt.update();
+        }
+    });
+}
+
+#[bench]
+fn many_to_one(b: &mut test::Bencher) {
+    let mut rt = sigmut::core::Runtime::new();
+    let mut ss = Vec::new();
+    for i in 0..10000 {
+        ss.push(sigmut::State::new(i));
+    }
+    let ss = Rc::new(ss);
+    let _sub = effect({
+        let ss = ss.clone();
+        move |sc| {
+            let mut x = 0;
+            for s in &*ss {
+                x += s.get(sc);
+            }
+            black_box(x);
+        }
+    });
+    b.iter(|| {
+        for n in 0..5 {
+            ss[n].set(n + 1, rt.ac());
+            rt.update();
+        }
+    });
+}
+
+#[bench]
+fn many_to_many(b: &mut test::Bencher) {
+    let mut rt = sigmut::core::Runtime::new();
+    let mut ss = Vec::new();
+    for i in 0..100 {
+        ss.push(sigmut::State::new(i));
+    }
+    let ss = Rc::new(ss);
+    let mut subs = Vec::new();
+    for j in 0..100 {
+        subs.push(effect({
+            let ss = ss.clone();
+            move |sc| {
+                let mut x = 0;
+                for s in &*ss {
+                    x += s.get(sc);
+                }
+                black_box((j, x));
+            }
+        }));
+    }
+    b.iter(|| {
+        for n in 0..5 {
+            ss[n].set(n + 1, rt.ac());
             rt.update();
         }
     });
