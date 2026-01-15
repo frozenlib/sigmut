bca9ad1d8c958fced4ca0d48918d9af5a9344572
frozenlib <frozenlib@users.noreply.github.com>
2026-01-16 02:18:27 +0900

    Remove `Action` and `Task` from core module exports in `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,8 @@ pub mod tests_readme {}
 
 #[doc(inline)]
 pub use crate::core::{
-    Action, ActionContext, ActionKind, AsyncActionContext, AsyncSignalContext, SignalContext,
-    StateRef, StateRefBuilder, Task, TaskKind, spawn_action, spawn_action_async, spawn_action_rc,
+    ActionContext, AsyncActionContext, AsyncSignalContext, SignalContext, StateRef,
+    StateRefBuilder, TaskKind, spawn_action, spawn_action_async, spawn_action_rc,
 };
 
 #[doc(inline)]
